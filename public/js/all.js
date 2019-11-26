455d53286927ac1e4bf8bc5006876fe9c4a385fe
tatdt622989 <70212cbkj67@gmail.com>
2019-11-27 01:21:30 +0800

    works arrange & works input

--- public/js/all.js
+++ public/js/all.js
@@ -1,7 +1,7 @@
 "use strict";
 
 $(document).ready(function () {
-  var allPage = ['#index-nav', '.intro-aboutme-wrap', '.website-wrap', '.threedcg-wrap'];
+  var allPage = ['#index-nav', '.intro-aboutme-wrap', '.website-wrap', '.threeDCG-wrap'];
   var thisPage = '';
   var i = 0; //抓取目前所在頁面
 
@@ -138,14 +138,14 @@ $(document).ready(function () {
       $('.nav-item').eq(1).css('background-color', '#e2e2e2');
       console.log($('.worksImg, .worksHover a').eq(3));
       $('.worksImg, .worksHover a').hover(function (e) {
-        var focus = parseInt(e.target.dataset.hover);
+        var focus = parseInt(e.target.dataset.order);
         console.log(focus);
         $('.worksHover').eq(focus).css({
           'opacity': '1',
           'visibility': 'visible'
         });
       }, function (e) {
-        var focus = parseInt(e.target.dataset.hover);
+        var focus = parseInt(e.target.dataset.order);
         console.log(focus);
         $('.worksHover').eq(focus).css({
           'opacity': '0',
@@ -154,17 +154,17 @@ $(document).ready(function () {
       });
       break;
 
-    case '.threedcg-wrap':
+    case '.threeDCG-wrap':
       $('.nav-item').eq(2).css('background-color', '#e2e2e2');
-      $('.worksImg, .threedcg-wrap button').hover(function (e) {
-        var focus = parseInt(e.target.dataset.hover);
+      $('.worksImg, .threeDCG-wrap button').hover(function (e) {
+        var focus = parseInt(e.target.dataset.order);
         console.log(focus);
         $('.worksHover').eq(focus).css({
           'opacity': '1',
           'visibility': 'visible'
         });
       }, function (e) {
-        var focus = parseInt(e.target.dataset.hover);
+        var focus = parseInt(e.target.dataset.order);
         console.log(focus);
         $('.worksHover').eq(focus).css({
           'opacity': '0',
@@ -179,7 +179,84 @@ $(document).ready(function () {
 
       getHeightFn();
       $(window).resize(getHeightFn);
+
+      var openExpandFn = function openExpandFn(e) {
+        var nodeName = e.target.nodeName;
+        var targetNum = parseInt(e.target.dataset.order);
+        var mqlLg = window.innerWidth;
+        var frameStr = '<img src="' + threeDCGLarge[targetNum].imgUrl[0] + '" alt="' + threeDCGLarge[targetNum].title + '">' + '<div class="img-status"></div>';
+        var slideStr = '';
+        var i = 0;
+
+        while (i < threeDCGPreview.length) {
+          slideStr += '<img src="' + threeDCGPreview[i] + '" alt="previewImg">';
+          ++i;
+        }
+
+        console.log(threeDCGLarge);
+        console.log(nodeName);
+        console.log(targetNum);
+        $('.img-frame').html(frameStr);
+        $('.slide-works').html(slideStr);
+
+        if (mqlLg >= 1364 && nodeName == 'BUTTON' || mqlLg < 1364 && nodeName == "IMG") {
+          console.log('match');
+          $('.expand-wrap').css({
+            'visibility': 'visible',
+            'opacity': '1'
+          });
+        } else {
+          return;
+        }
+      };
+
+      var closeExpandFn = function closeExpandFn() {
+        $('.expand-wrap').css({
+          'visibility': 'hidden',
+          'opacity': '0'
+        });
+      };
+
+      $('.threeDCG-wrap .row').eq(1).click(openExpandFn);
+      $('.close-btn').click(closeExpandFn);
+      console.log($('.threeDCG-wrap .row'));
       break;
   }
 });
+"use strict";
+
+var threeDCGLarge = [{
+  title: '角色設計練習',
+  tools: 'Maya,&nbsp;PhotoShop',
+  imgUrl: ['./images/3dCh01-lg.jpg', './images/3dCh02-lg.jpg', './images/3dCh03-lg.jpg']
+}, {
+  title: '歐風木屋',
+  tools: 'Maya,&nbsp;PhotoShop',
+  imgUrl: ['./images/house01-lg.jpg', './images/house02-lg.jpg']
+}, {
+  title: '安妮公主花園吉祥物',
+  tools: 'Maya,&nbsp;PhotoShop',
+  imgUrl: ['./images/princess-lg.jpg']
+}, {
+  title: '音樂廳',
+  tools: 'Maya',
+  imgUrl: ['./images/concert-lg.jpg']
+}, {
+  title: '啤酒罐場景',
+  tools: 'Maya,&nbsp;PhotoShop',
+  imgUrl: ['./images/beer-lg.jpg']
+}, {
+  title: '世名無動畫-瓢蟲',
+  tools: 'Maya,&nbsp;PhotoShop',
+  imgUrl: ['./images/ladybird01-lg.jpg', './images/ladybird02-lg.jpg']
+}, {
+  title: '酒瓶場景',
+  tools: 'Maya,&nbsp;PhotoShop',
+  imgUrl: ['./images/liqueur01-lg.jpg', './images/liqueur02-lg.jpg', './images/liqueur03-lg.jpg']
+}, {
+  title: '聖誕節卡片',
+  tools: 'Maya',
+  imgUrl: ['./images/2018card-lg.jpg']
+}];
+var threeDCGPreview = ['./images/3dCh01-preview.jpg', './images/3dCh02-preview.jpg', './images/3dCh03-preview.jpg', './images/house01-preview.jpg', './images/house02-preview.jpg', './images/princess-preview.jpg', './images/beer-preview.jpg', './images/ladybird01-preview.jpg', './images/ladybird02-preview.jpg', './images/liqueur01-preview.jpg', './images/liqueur02-preview.jpg', './images/liqueur03-preview.jpg', './images/2018card-preview.jpg'];
 //# sourceMappingURL=all.js.map
